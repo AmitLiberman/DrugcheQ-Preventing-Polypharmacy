d88cc3740e94307c37e0a8c9960a61b7bb2e1da2
Amit Liberman <amitliberman224@gmail.com>
2020-12-14 13:09:06 +0200

    actions navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import TopNav from "./Components/TopNav";
+import ActionNav from "./Components/ActionNav";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = (props) => {
   return (
     <div>
       <TopNav />
+      <ActionNav />
       <Router>
         <div>
           <Switch>
@@ -14,6 +17,16 @@ const App = (props) => {
             </Route>
             <Route path="/topics">
               <Topics />
+            </Route>
+
+            <Route path="/interaction-checker">
+              <InteractionChecker />
+            </Route>
+            <Route path="/side-effect-report">
+              <SideEffectReport />
+            </Route>
+            <Route path="/drug-search">
+              <DrugSearch />
             </Route>
             <Route path="/">
               <Home />
@@ -35,6 +48,18 @@ function About() {
 
 function Topics() {
   return <h2>Topics</h2>;
+}
+
+function InteractionChecker() {
+  return <h2>Interaction Checker</h2>;
+}
+
+function SideEffectReport() {
+  return <h2>Side Effect Report</h2>;
+}
+
+function DrugSearch() {
+  return <h2>Drug Search</h2>;
 }
 
 export default App;
--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
-
-// TO DO
-// Replace the inline style with style-component
-
+import "./TopNav.css";
+// top navigation bar
 const TopNav = (props) => {
   return (
     <div>
       <Navbar
-        style={{ direction: "rtl" }}
+        className="nav-bar"
         collapseOnSelect
         expand="lg"
         bg="light"
         variant="light"
       >
-        <Navbar.Brand href="/">DrugcheQ</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle
+          className="hamburger-nav"
+          aria-controls="responsive-navbar-nav"
+        />
+
+        <Navbar.Brand clssName="logo-wrapper" href="/">
+          <h2 className="drugheq-logo">DrugcheQ</h2>
+        </Navbar.Brand>
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav style={{ marginRight: "20px" }} className="ml-auto">
-            <Nav.Link href="/">בית</Nav.Link>
-            <Nav.Link href="/about">אודות</Nav.Link>
-            <Nav.Link href="/topics">נושאים</Nav.Link>
+          <Nav className="ml-auto top-nav-content">
+            <Nav.Link className="top-nav-link" href="/">
+              <span className="link-text">בית</span>
+            </Nav.Link>
+            <Nav.Link className="top-nav-link" href="/about">
+              <span className="link-text">אודות</span>
+            </Nav.Link>
+            <Nav.Link className="top-nav-link" href="/topics">
+              <span className="link-text">נושאים</span>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
