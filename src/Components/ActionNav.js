06646c0fd108a24dd1233e0f7d2d7aa222914ff9
Amit Liberman <amitliberman224@gmail.com>
2020-12-17 10:28:58 +0200

    make eject

--- src/Components/ActionNav.js
+++ src/Components/ActionNav.js
@@ -6,17 +6,17 @@ const ActionNav = (props) => {
   return (
     <div className="action-nav-wrapper">
       <div className="action-opt">
-        <Nav.Link href="/drug-search">
+        <Nav.Link className="nav-link" href="/drug-search">
           <span className="action-text"> חיפוש תרופה</span>
         </Nav.Link>
       </div>
       <div className="action-opt">
-        <Nav.Link href="/interaction-checker">
+        <Nav.Link className="nav-link" href="/interaction-checker">
           <span className="action-text">בדיקת אינטראקציה</span>
         </Nav.Link>
       </div>
       <div className="action-opt">
-        <Nav.Link href="/side-effect-report">
+        <Nav.Link className="nav-link" href="/side-effect-report">
           <span className="action-text">דיווח על תופעות לוואי</span>
         </Nav.Link>
       </div>
