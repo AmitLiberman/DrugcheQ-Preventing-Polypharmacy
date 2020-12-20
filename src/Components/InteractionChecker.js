9825e812a374a8bb374c9ff8853f110c0bd9f9a8
Amit Liberman <amitliberman224@gmail.com>
2020-12-20 11:27:38 +0200

    save and submit buttons added to interaction checker

--- src/Components/InteractionChecker.js
+++ src/Components/InteractionChecker.js
@@ -4,20 +4,24 @@ import DrugList from "./DrugList";
 
 class InteractionChecker extends Component {
   state = {
-    drugName: "",
+    drugName: "", //drug name that submited in input
     drugList: [],
   };
 
+  //Submit Drug Item to list
   handleSubmit = (event) => {
     event.preventDefault();
     const newDrugItem = {
       id: this.state.drugList.length + 1,
       name: this.state.drugName,
     };
-    this.setState({ drugList: [...this.state.drugList, newDrugItem] });
-    this.setState({ drugName: "" });
+    if (newDrugItem.name.trim().length !== 0) {
+      //if the input not contains only spaces
+      this.setState({ drugList: [...this.state.drugList, newDrugItem] });
+      this.setState({ drugName: "" });
+    }
   };
-
+  //Change State to the drug name that typed
   handleChange = (event) => {
     this.setState({ drugName: event.target.value });
   };
@@ -30,6 +34,18 @@ class InteractionChecker extends Component {
   };
 
   render() {
+    let checkIntreactionBtn = "";
+    if (this.state.drugList.length >= 1) {
+      checkIntreactionBtn = (
+        <div className="submit-drug-list-btn-container">
+          <button className="btn btn-primary btn-lg check-btn">
+            בדוק אינטראקציה
+          </button>
+          <button className="btn btn-light save-btn">שמור</button>
+        </div>
+      );
+    }
+
     return (
       <div className="interaction-container">
         <form className="interaction-form" onSubmit={this.handleSubmit}>
@@ -40,11 +56,14 @@ class InteractionChecker extends Component {
             onChange={this.handleChange}
             type="text"
             className="input-drug-name"
-            placeholder="הכנסת שם תרופה בעברית או באנגלית"
+            placeholder="הכנס/י שם תרופה בעברית או באנגלית"
             value={this.state.drugName}
           />
         </form>
-        <DrugList delDrug={this.delDrug} drugList={this.state.drugList} />
+        <div className="drug-list-container">
+          <DrugList delDrug={this.delDrug} drugList={this.state.drugList} />
+        </div>
+        {checkIntreactionBtn}
       </div>
     );
   }
