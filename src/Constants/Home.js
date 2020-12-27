3e564ed0f556ca2b210488cd14d226bab367b408
Amit Liberman <amitliberman224@gmail.com>
2020-12-27 17:00:23 +0200

    constants folder added

---
+++ src/Constants/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import "./Home.css";
+
+const Home = (props) => {
+  return (
+    <React.Fragment>
+      <p>אזור חופשי</p>
+      <p>
+        בחלק זה יהיה ניתן להסביר את תופעת ריבוי התרופות, לפרט על הסיכונים,
+        הבעיות ודרכי ההתמודדות. כמו כן, הסבר כללי על המערכת ולמה חשוב להשתמש בה
+        ולבסוף ניתן יהיה להוסיף תנאי שימוש
+      </p>
+    </React.Fragment>
+  );
+};
+
+export default Home;
---
+++ src/Constants/TopNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import "./TopNav.css";
+// top navigation bar
+const TopNav = (props) => {
+  return (
+    <div>
+      <Navbar
+        className="nav-bar"
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+      >
+        <Navbar.Toggle
+          className="hamburger-nav"
+          aria-controls="responsive-navbar-nav"
+        />
+
+        <Navbar.Brand className="logo-wrapper" href="/">
+          <h2 className="drugheq-logo">DrugcheQ</h2>
+        </Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto top-nav-content">
+            <Nav.Link className="top-nav-link" href="/">
+              <span className="link-text">בית</span>
+            </Nav.Link>
+            <Nav.Link className="top-nav-link" href="/about">
+              <span className="link-text">אודות</span>
+            </Nav.Link>
+            <Nav.Link className="top-nav-link" href="/connect">
+              <span className="link-text">צור קשר</span>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default TopNav;
