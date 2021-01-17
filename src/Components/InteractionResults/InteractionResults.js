9835254a3c4c8eff20dc5db06fe804d2c116141e
Amit Liberman <amitliberman224@gmail.com>
2021-01-17 13:15:11 +0200

    added severity for interaction checker

--- src/Components/InteractionResults/InteractionResults.js
+++ src/Components/InteractionResults/InteractionResults.js
@@ -6,27 +6,21 @@ class InteractionResults extends Component {
     let results = [];
     const resultsLength = Object.keys(this.props.results).length;
     for (let index = 0; index < resultsLength; index++) {
+      let severit = "";
+
+      if (this.props.results[index].severity != null) {
+        severit = <h6 className="highSeverity">High Risk</h6>;
+      }
       results.push(
         <div className="drugInteractionElement">
           <h3>
             {this.props.results[index].drug1} -{this.props.results[index].drug2}
           </h3>
           <h6>{this.props.results[index].description}</h6>
+          {severit}
         </div>
       );
     }
-
-    // console.log(results);
-    // this.props.results.forEach((element) => {
-    //   results.push(
-    //     <div className="drugInteractionElement">
-    //       <h3>
-    //         {element.drug1} - {element.drug2}
-    //       </h3>
-    //       <h6>{element.description}</h6>
-    //     </div>
-    //   );
-    // });
 
     return <div>{results}</div>;
   }
