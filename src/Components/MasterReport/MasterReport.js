d20334e71060c5f53e57237525b737e0185a58e7
Amit Liberman <amitliberman224@gmail.com>
2021-01-31 13:46:18 +0200

    added next and prev to the temp form

--- src/Components/MasterReport/MasterReport.js
+++ src/Components/MasterReport/MasterReport.js
@@ -6,18 +6,18 @@ import Step3 from "../Step3/Step3";
 class MasterReport extends Component {
   state = {
     currentStep: 1, // Default is Step 1
-    name: "",
+    username: "",
     email: "",
-    sector: "",
+    password: "",
   };
 
   // Use the submitted data to set the state
-  handleChange(event) {
+  handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
     });
-  }
+  };
 
   // Trigger an alert on form submission
   handleSubmit = (event) => {
@@ -28,6 +28,60 @@ class MasterReport extends Component {
     Username: ${username} \n
     Password: ${password}`);
   };
+
+  _next = () => {
+    let currentStep = this.state.currentStep;
+    // If the current step is 1 or 2, then add one on "next" button click
+    currentStep = currentStep >= 2 ? 3 : currentStep + 1;
+    this.setState({
+      currentStep: currentStep,
+    });
+  };
+
+  _prev = () => {
+    let currentStep = this.state.currentStep;
+    // If the current step is 2 or 3, then subtract one on "previous" button click
+    currentStep = currentStep <= 1 ? 1 : currentStep - 1;
+    this.setState({
+      currentStep: currentStep,
+    });
+  };
+
+  get previousButton() {
+    let currentStep = this.state.currentStep;
+    // If the current step is not 1, then render the "previous" button
+    if (currentStep !== 1) {
+      return (
+        <button
+          className="btn btn-secondary"
+          type="button"
+          onClick={this._prev}
+        >
+          Previous
+        </button>
+      );
+    }
+    // ...else return nothing
+    return null;
+  }
+
+  get nextButton() {
+    let currentStep = this.state.currentStep;
+    // If the current step is not 3, then render the "next" button
+    if (currentStep < 3) {
+      return (
+        <button
+          className="btn btn-primary float-right"
+          type="button"
+          onClick={this._next}
+        >
+          Next
+        </button>
+      );
+    }
+    // ...else render nothing
+    return null;
+  }
 
   render() {
     return (
@@ -50,6 +104,8 @@ class MasterReport extends Component {
             handleChange={this.handleChange}
             password={this.state.password}
           />
+          {this.previousButton}
+          {this.nextButton}
         </form>
       </React.Fragment>
     );
