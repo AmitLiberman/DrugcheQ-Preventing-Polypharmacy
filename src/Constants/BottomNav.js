cae356241ce08a925db2aa8e97097240bd0cbe28
Amit Liberman <amitliberman224@gmail.com>
2021-02-28 12:38:19 +0200

    bottom navigation bar is finished for Desktop

--- src/Constants/BottomNav.js
+++ src/Constants/BottomNav.js
@@ -4,7 +4,7 @@ import "./BottomNav.css";
 const BottomNav = (props) => {
   return (
     <div className="bottom-nav-container">
-      <div className="action-categories ">
+      <div className="category-wrapper actions">
         <h6 className="category-name">פעולות</h6>
         <a className="bottom-action-nav-link" href="/drug-search">
           <span className="bottom-link-text"> חיפוש תרופה</span>
@@ -14,6 +14,28 @@ const BottomNav = (props) => {
         </a>
         <a className="bottom-action-nav-link" href="/side-effect-report">
           <span className="bottom-link-text">דיווח על תופעות לוואי</span>
+        </a>
+      </div>
+      <div className="category-wrapper support">
+        <h6 className="category-name">תמיכה</h6>
+        <a className="bottom-action-nav-link" href="/connect">
+          <span className="bottom-link-text">צור קשר</span>
+        </a>
+        <a className="bottom-action-nav-link" href="/faq">
+          <span className="bottom-link-text">שאלות ותשובות</span>
+        </a>
+        <a className="bottom-action-nav-link" href="/terms">
+          <span className="bottom-link-text">תנאי שימוש</span>
+        </a>
+      </div>
+
+      <div className="category-wrapper support">
+        <h6 className="category-name">משתמשים</h6>
+        <a className="bottom-action-nav-link" href="/connect">
+          <span className="bottom-link-text">התחבר</span>
+        </a>
+        <a className="bottom-action-nav-link" href="/faq">
+          <span className="bottom-link-text">הרשם</span>
         </a>
       </div>
     </div>
