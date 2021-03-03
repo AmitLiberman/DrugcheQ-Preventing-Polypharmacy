03e124d13935e0711370a0ef75663ffe49f93500
Amit Liberman <amitliberman224@gmail.com>
2021-03-03 19:29:50 +0200

    added Accordion component from bootstrap

--- src/Components/DrugSearch/DrugSearch.js
+++ src/Components/DrugSearch/DrugSearch.js
@@ -1,44 +1,11 @@
 import React, { Component } from "react";
-import "./DrugSearch.css";
+import DrugSearchResults from "../DrugSearchResults/DrugSearchResults";
 
 class DrugSearch extends Component {
-  state = {
-    drugName: "", //drug name that submited in input
-  };
-
-  //Change State to the drug name that typed
-  handleChange = (event) => {
-    this.setState({ drugName: event.target.value });
-  };
-
-  handleSubmit = () => {
-    alert(this.state.drugName);
-  };
   render() {
     return (
-      <div className="drug-search-container">
-        <div className="drug-search-describe-container">
-          <h2>חיפוש תרופה</h2>
-          <p>התחל להקליד שם תרופה ובחר את ההתאמה הטובה ביותר מרשימת ההצעות.</p>
-        </div>
-        <form className="interaction-form" onSubmit={this.handleSubmit}>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <button className="btn btn-info add-drug-btn" type="submit">
-                הוסף
-              </button>
-            </div>
-            <input
-              onChange={this.handleChange}
-              type="text"
-              className="form-control input-drug-name-search"
-              placeholder="הכנס/י שם תרופה"
-              value={this.state.drugName}
-              aria-label=""
-              aria-describedby="basic-addon1"
-            />
-          </div>
-        </form>
+      <div>
+        <DrugSearchResults />
       </div>
     );
   }
---
+++ src/Components/DrugSearchResults/DrugSearchResults.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+
+class DrugSearchResults extends Component {
+  state = {
+    drugName: "", //drug name that submited in input
+  };
+
+  render() {
+    return (
+      <Accordion>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              Click me!
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+              Click me!
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    );
+  }
+}
+
+export default DrugSearchResults;
