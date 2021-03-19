6bd29d66747c894f646870bac5e09678179a8394
Amit Liberman <amitliberman224@gmail.com>
2021-03-19 11:09:02 +0200

    fixed css

--- src/Components/ModalBox/ModalBox.js
+++ src/Components/ModalBox/ModalBox.js
@@ -7,7 +7,8 @@ class ModalBox extends Component {
       <div className="modal-box-container">
         <div className="modal-content-text">
           <span className="close-modal-box">&times;</span>
-          <p>Some text in the box</p>
+          <h5> הנתונים נקלטו במערכת</h5>
+          <h5> תודה על הדיווח</h5>
         </div>
       </div>
     );
