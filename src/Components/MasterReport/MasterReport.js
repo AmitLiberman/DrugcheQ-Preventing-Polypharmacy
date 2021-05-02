5e1bf975919c6da753466f927b76a6d047ae8750
Amit Liberman <amitliberman224@gmail.com>
2021-05-02 16:59:24 +0300

    deploy to firebase

--- src/Components/MasterReport/MasterReport.js
+++ src/Components/MasterReport/MasterReport.js
@@ -70,7 +70,7 @@ class MasterReport extends Component {
     });
     // axios.post("https://drugcheq.herokuapp.com/side-effect-report", json);
 
-    axios.post("http://127.0.0.1:5000/side-effect-report", json);
+    axios.post("https://drugcheq.herokuapp.com/side-effect-report", json);
   };
 
   updateDrugListIds = (newDrugList) => {
