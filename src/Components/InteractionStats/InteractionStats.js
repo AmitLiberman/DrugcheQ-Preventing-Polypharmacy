16453a6d29e6ff9a4dee3448815637c3205775e8
Amit Liberman <amitliberman224@gmail.com>
2021-05-11 15:07:55 +0300

    Fixed CSS

--- src/Components/InteractionStats/InteractionStats.js
+++ src/Components/InteractionStats/InteractionStats.js
@@ -27,13 +27,12 @@ class InteractionStats extends Component {
   };
   render() {
     return (
-      <div className="interaction-and-chart-cont">
+      <div className="interaction-stats-chart">
         <div className="no-chart-data">
           <h6>80 דיווחים מכילים את התרופות aspirin, coumadin, optalgin</h6>
           <h6>80% דיווחו על תופעות לוואי בדרגה קלה</h6>
           <h6>20% דיווחו על תופעות לוואי בדרגה קשה</h6>
         </div>
-
         <div className="pie-chart-container">
           <Pie
             className="pie-chart"
