eb0702107fd2e5b7f90e94ee98800a51ce85dfc6
Amit Liberman <amitliberman224@gmail.com>
2021-05-27 10:46:37 +0300

    added form for new drug

--- src/Components/NewDrug/NewDrug.js
+++ src/Components/NewDrug/NewDrug.js
@@ -1,149 +1,48 @@
 import React, { Component } from "react";
+import "./NewDrug.css";
 
 class NewDrug extends Component {
   render() {
     return (
-      <div className="form-group1">
-        <div className="main-sectors-container">
-          <div
-            className="sector-container"
-            onChange={(event) => this.onSectorChange(event)}
-          >
-            <h5>
-              סקטור מדווח
-              <span style={{ color: "red", fontWeight: "bold" }}>*</span>
-            </h5>
-            <label className="radio-option">
-              ציבור
-              <input
-                className="radio-input"
-                type="radio"
-                name="sector"
-                value="public"
-                checked={this.props.sector === "public"}
-              />
-            </label>
-            <label className="radio-option">
-              צוות רפואי
-              <input
-                className="radio-input"
-                type="radio"
-                name="sector"
-                value="medical"
-                checked={this.props.sector === "medical"}
-              />
-            </label>
-            <label className="radio-option">
-              אחר
-              <input
-                className="radio-input"
-                type="radio"
-                name="sector"
-                value="other"
-                checked={this.props.sector === "other"}
-              />
-            </label>
-          </div>
-          {this.props.isMedicalSector ? (
-            <div
-              className="sector-container specific"
-              onChange={(event) => this.onMedicalSectorChange(event)}
-            >
-              <h5>
-                בחר סקטור
-                <span style={{ color: "red", fontWeight: "bold" }}>*</span>
-              </h5>
-              <label className="radio-option">
-                רפואה
-                <input
-                  className="radio-input"
-                  type="radio"
-                  name="medicalSector"
-                  value="medicine"
-                  checked={this.props.medicalSector === "medicine"}
-                />
-              </label>
-              <label className="radio-option">
-                רוקחות
-                <input
-                  className="radio-input"
-                  type="radio"
-                  name="medicalSector"
-                  value="pharmacy"
-                  checked={this.props.medicalSector === "pharmacy"}
-                />
-              </label>
-              <label className="radio-option">
-                סיעוד
-                <input
-                  className="radio-input"
-                  type="radio"
-                  name="medicalSector"
-                  value="nursing"
-                  checked={this.props.medicalSector === "nursing"}
-                />
-              </label>
-              <label className="radio-option">
-                אחר
-                <input
-                  className="radio-input"
-                  type="radio"
-                  name="medicalSector"
-                  value="other"
-                  checked={this.props.medicalSector === "other"}
-                />
-              </label>
-            </div>
-          ) : null}
-        </div>
-        <div className="personal-detail-container">
-          <label className="step1-lable" htmlFor="factorName">
-            שם גורם{" "}
-            <span style={{ fontWeight: "bold", fontSize: "1rem" }}>
-              (לא חובה)
-            </span>
+      <form className="main-drug-suggest-form">
+        <div className="form-group">
+          <label className="new-drug-lable" for="commercialName">
+            שם מסחרי (עברית/אנגלית)
           </label>
           <input
-            className={this.props.userInputStyle}
-            id="factorName"
-            name="factorName"
             type="text"
-            placeholder="הכנס את שמך או את שם הארגון"
-            value={this.props.factorName} // Prop: The email input data
-            onChange={this.props.handleChange} // Prop: Puts data into state
-          />
-          <label className="step1-lable" htmlFor="email">
-            כתובת דואר אלקטרוני{" "}
-            <span style={{ fontWeight: "bold", fontSize: "1rem" }}>
-              (לא חובה)
-            </span>
-          </label>
-          <input
-            className={this.props.emailInputStyle}
-            id="email"
-            name="email"
-            type="text"
-            placeholder="example@mail.com"
-            value={this.props.email} // Prop: The email input data
-            onChange={this.props.handleChange} // Prop: Puts data into state
-          />
-          <label className="step1-lable" htmlFor="phoneNumber">
-            מספר טלפון{" "}
-            <span style={{ fontWeight: "bold", fontSize: "1rem" }}>
-              (לא חובה)
-            </span>
-          </label>
-          <input
-            className={this.props.emailInputStyle}
-            id="phoneNumber"
-            name="phoneNumber"
-            type="number"
-            placeholder="01-2345678"
-            value={this.props.phoneNumber} // Prop: The email input data
-            onChange={this.props.handleChange} // Prop: Puts data into state
+            className="form-control"
+            id="commercialHebrew"
+            placeholder="שם מסחרי"
           />
         </div>
-      </div>
+        <div className="form-group">
+          <label className="new-drug-lable" for="genericName">
+            שם גנרי (עברית/אנגלית)
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="genericName"
+            placeholder="שם גנרי"
+          />
+        </div>
+        <div className="form-group">
+          <label className="new-drug-lable" for="useForm">
+            דרך מתן/שימוש
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="useForm"
+            placeholder="כדורים/זריקה/משחה וכו'"
+          />
+        </div>
+
+        <button type="submit" className="send-new-btn">
+          שליחה
+        </button>
+      </form>
     );
   }
 }
